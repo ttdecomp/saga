51761d644b1e96acfad3ed45fbab1e509336d69b
Seán de Búrca <sdeburca@fastmail.net>
2026-01-08 09:59:24 -0800

    Move string functions to C++ with extern "C"

--- src/nu2api.saga/nucore/nustring.h
+++ src/nu2api.saga/nucore/nustring.h
@@ -3,8 +3,18 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void NuStrCat(char *str, const char *ext);
+char *NuStrChr(char *src, char c);
 size_t NuStrCpy(char *dest, const char *src);
 size_t NuStrLen(const char *str);
 int32_t NuStrNICmp(const char *a, const char *b, size_t n);
+unsigned char NuToLower(unsigned char c);
 unsigned char NuToUpper(unsigned char c);
+
+#ifdef __cplusplus
+}
+#endif
