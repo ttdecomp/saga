7e6b1e6028278ea44ba23601814888622fba7f00
Seán de Búrca <sdeburca@fastmail.net>
2026-01-08 14:52:46 -0800

    String function accuracy improvements

--- src/nu2api.saga/nucore/nustring.h
+++ src/nu2api.saga/nucore/nustring.h
@@ -8,10 +8,16 @@
 C_API_START
 
 void NuStrCat(char *str, const char *ext);
+int NuStrCmp(const char *a, const char *b);
 char *NuStrChr(char *src, char c);
 size_t NuStrCpy(char *dest, const char *src);
+void NuStrFixExtPlatform(char *dst, char *src, char *ext, int dst_size, char *platform_string);
+int NuStrICmp(const char *a, const char *b);
 size_t NuStrLen(const char *str);
-int32_t NuStrNICmp(const char *a, const char *b, size_t n);
+int NuStrNCmp(const char *a, const char *b, size_t n);
+int NuStrNICmp(const char *a, const char *b, size_t n);
+int NuStrNCpy(char *dest, const char *src, int n);
+char *NuStrRChr(char *src, char c);
 unsigned char NuToLower(unsigned char c);
 unsigned char NuToUpper(unsigned char c);
 
--- src/nu2api.saga/nucore/nustring_c.cpp
+++ src/nu2api.saga/nucore/nustring_c.cpp
@@ -19,52 +19,203 @@ void NuStrCat(char *str, const char *ext) {
     }
 }
 
-char *NuStrChr(char *src, char c) {
+int NuStrCmp(const char *a, const char *b) {
+    char c1;
+    char c2;
+
+    if (a == NULL) {
+        return -1;
+    }
+
+    if (b == NULL) {
+        return 1;
+    }
+
     do {
-        if (*src == '\0') {
-            return NULL;
+        c1 = *a;
+        c2 = *b;
+
+        if (c1 > c2) {
+            return 1;
+        }
+
+        if (c1 < c2) {
+            return -1;
+        }
+
+        a += 1;
+        b += 1;
+    } while (c1 != '\0' && c2 != '\0');
+
+    return 0;
+}
+
+char *NuStrChr(char *src, char c) {
+    while (*src != '\0') {
+        if (*src == c) {
+            return src;
         }
 
         src += 1;
-    } while (*src != c);
+    }
 
-    return src;
+    return NULL;
 }
 
-size_t NuStrCpy(char *dest, const char *src) {
-    char *orig = dest;
+size_t NuStrCpy(char *dst, const char *src) {
+    char *orig = dst;
 
     if (src != NULL) {
-        for (; *src != '\0'; src = src + 1) {
-            *dest = *src;
-            dest = dest + 1;
+        for (; *src != '\0'; src += 1) {
+            *dst = *src;
+            dst = dst + 1;
         }
     }
 
-    *dest = '\0';
+    *dst = '\0';
 
-    return (size_t)dest - (size_t)orig;
+    return dst - orig;
+}
+
+void NuStrFixExtPlatform(char *dst, char *src, char *ext, int dst_size, char *platform_string) {
+    char *p;
+    char *sep;
+    char *sep2;
+
+    if (src == NULL || ext == NULL || dst == NULL) {
+        return;
+    }
+
+    NuStrCpy(dst, src);
+
+    p = NuStrRChr(dst, '.');
+    sep = NuStrRChr(dst, '/');
+    sep2 = NuStrRChr(dst, '\\');
+
+    if (sep2 > sep) {
+        sep = sep2;
+    }
+
+    if (sep >= p) {
+        NuStrCat(dst, "_");
+
+        if (platform_string != NULL) {
+            NuStrCat(dst, platform_string);
+        }
+
+        NuStrCat(dst, ".");
+        NuStrCat(dst, ext);
+    } else {
+        *p = '\0';
+        NuStrCat(p, "_");
+
+        if (platform_string != NULL) {
+            NuStrCat(p, platform_string);
+        }
+
+        NuStrCat(p, ".");
+        NuStrCat(p, ext);
+    }
+
+    NuStrLen(dst);
+}
+
+int NuStrICmp(const char *a, const char *b) {
+    char c1;
+    char c2;
+
+    if (a == NULL) {
+        return -1;
+    }
+
+    if (b == NULL) {
+        return 1;
+    }
+
+    do {
+        c1 = NuToUpper(*a);
+        c2 = NuToUpper(*b);
+
+        if (c1 > c2) {
+            return 1;
+        }
+
+        if (c1 < c2) {
+            return -1;
+        }
+
+        a += 1;
+        b += 1;
+    } while (c1 != '\0' && c2 != '\0');
+
+    return 0;
 }
 
 size_t NuStrLen(const char *str) {
     size_t i = 0;
 
-    for (; *str != '\0'; str = str + 1) {
+    for (; *str != '\0'; str += 1) {
         i++;
     }
 
     return i;
 }
 
-int32_t NuStrNICmp(const char *a, const char *b, size_t n) {
+int NuStrNCmp(const char *a, const char *b, size_t n) {
     char c1;
     char c2;
 
     if (a == NULL) {
         return -1;
-    } else if (b == NULL) {
+    }
+
+    if (b == NULL) {
         return 1;
-    } else if (n == 0) {
+    }
+
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == -1) {
+        n = NuStrLen(a);
+    } else if (n == -2) {
+        n = NuStrLen(b);
+    }
+
+    do {
+        c1 = *a;
+        c2 = *b;
+
+        if (c1 > c2) {
+            return 1;
+        }
+
+        if (c1 < c2) {
+            return -1;
+        }
+
+        a++;
+        b++;
+        n--;
+    } while (c1 != '\0' && c2 != '\0' && n != 0);
+
+    return 0;
+}
+
+int NuStrNICmp(const char *a, const char *b, size_t n) {
+    char c1;
+    char c2;
+
+    if (a == NULL) {
+        return -1;
+    }
+
+    if (b == NULL) {
+        return 1;
+    }
+
+    if (n == 0) {
         return 0;
     }
 
@@ -89,13 +240,53 @@ int32_t NuStrNICmp(const char *a, const char *b, size_t n) {
         a++;
         b++;
         n--;
-
-        if (c1 == '\0' || c2 == '\0') {
-            break;
-        }
-    } while (n != 0);
+    } while (c1 != '\0' && c2 != '\0' && n != 0);
 
     return 0;
+}
+
+int NuStrNCpy(char *dst, const char *src, int n) {
+    int count = 0;
+
+    if (src == NULL) {
+        *dst = '\0';
+        return 0;
+    }
+
+    for (; *src != '\0'; src += 1) {
+        n -= 1;
+        if (n < 1) {
+            *dst = '\0';
+            count += 1;
+
+            break;
+        }
+
+        *dst = *src;
+        dst = dst + 1;
+
+        count += 1;
+    }
+
+    return count - 1;
+}
+
+char *NuStrRChr(char *src, char c) {
+    char *ptr = src;
+
+    while (*ptr != '\0') {
+        ptr += 1;
+    }
+
+    while (ptr >= src) {
+        if (*ptr == c) {
+            return ptr;
+        }
+
+        ptr -= 1;
+    }
+
+    return NULL;
 }
 
 unsigned char NuToLower(unsigned char c) {
