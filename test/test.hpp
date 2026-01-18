bcef89d8b2c50f94f8a861c43eeacdf2ccfef1ae
Fabian Lippold <fabianlippold1184@gmail.com>
2026-01-18 20:59:55 +0100

    try to fix gh actions build

--- test/test.hpp
+++ test/test.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <print>
-#include <stdexcept>
+#include <format>
+#include <iostream>
 
 #define CHECK(condition, fmt, ...) __check_impl((condition), (fmt), #condition, __VA_ARGS__)
 
@@ -12,9 +12,12 @@ static void __check_impl(bool condition, const char *condition_str, std::format_
         std::string BOLD{"\33[1m"};
         std::string RESET{"\33[0m"};
 
-        std::print(stderr, "{}CHECK({}) failed:", BOLD + RED, condition_str);
-        std::println(stderr, fmt, std::forward<Args>(args)...);
-        std::print(stderr, "{}{} at {}:{} in {}{}\n", RESET, RED, __FILENAME__, __LINE__, __func__, RESET);
+        std::string buffer;
+        std::format_to(std::back_inserter(buffer), "{}CHECK({}) failed:", BOLD + RED, condition_str);
+        std::format_to(std::back_inserter(buffer), fmt, std::forward<Args>(args)...);
+        std::format_to(std::back_inserter(buffer), "{}{} at {}:{} in {}{}\n", RESET, RED, __FILENAME__, __LINE__,
+                       __func__, RESET);
+        std::cerr << buffer;
 
         std::exit(EXIT_FAILURE);
     }
