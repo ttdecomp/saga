a40d442e97c06020bfac31a8700041dc94ed847e
Avery Townsend <self@codeshaunted.com>
2026-01-31 16:25:58 -0500

    fix NuCameraCreate

--- src/nu2api.saga/nu3d/nucamera.cpp
+++ src/nu2api.saga/nu3d/nucamera.cpp
@@ -7,23 +7,20 @@ NUCAMERA *NuCameraCreate() {
 
     NuMtxSetIdentity(&cam->mtx);
 
-    cam->fov = 0.75f;
-    cam->aspect = 0.75f;
     cam->nearclip = 0.15f;
     cam->farclip = 10000.0f;
-
+    cam->rotation.x = 0.0f;
+    cam->fov = 0.75f;
+    cam->aspect = 0.75f;
+    cam->forward.z = 1.0f;
+    cam->right.x = 1.0f;
     cam->forward.x = 0.0f;
     cam->forward.y = 0.0f;
-    cam->forward.z = 1.0f;
-
-    cam->right.x = 1.0f;
-    cam->right.y = 0.0f;
-    cam->right.z = 0.0f;
-
     cam->rotation.w = 1.0f;
-    cam->rotation.x = 0.0f;
-    cam->rotation.y = 1.0f;
     cam->rotation.z = 1.0f;
+    cam->rotation.y = 1.0f;
+    cam->right.z = 0.0f;
+    cam->right.y = 0.0f;
 
     return cam;
 }
