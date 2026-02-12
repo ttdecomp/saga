50c6a540170d535fbbe672c33cf56b2f5f96a2bb
Seán de Búrca <sdeburca@fastmail.net>
2026-02-11 18:21:05 -0800

    Add AIStateFind

---
+++ src/gameapi.saga/ai/aisys/aistate.cpp
@@ -0,0 +1,21 @@
+#include "gameapi.saga/ai/aisys/aisys.h"
+#include "nu2api.saga/nucore/nulist.h"
+#include "nu2api.saga/nucore/nustring.h"
+
+AISTATE *AIStateFind(char *name, AISCRIPT *script) {
+    AISTATE *state;
+
+    state = (AISTATE *)NuLinkedListGetHead(&script->states);
+
+    if (name != NULL) {
+        while (state != NULL) {
+            if (NuStrICmp(name, state->name) == 0) {
+                return state;
+            }
+
+            state = (AISTATE *)NuLinkedListGetNext(&script->states, &state->list_node);
+        }
+    }
+
+    return NULL;
+}
