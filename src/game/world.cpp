c54f9b53759b488c54a3122ca47378bb1ac4ae9c
pasdalover <amazingemail@email.email>
2026-02-14 02:19:30 -0500

    Implement WorldInfo_Activate

--- src/game/world.cpp
+++ src/game/world.cpp
@@ -1,4 +1,5 @@
 #include "game/world.h"
+#include "nu2api.saga/nuandroid/ios_graphics.h"
 
 #include <string.h>
 
@@ -7,10 +8,22 @@ WORLDINFO *WORLD = &WorldInfo;
 
 /// @brief Pointer to the currently loading world info
 static WORLDINFO *LWORLD = &WorldInfo;
-
+extern bool g_BackgroundUsedFogColour;
 void WorldInfo_Activate(void) {
     WORLD = LWORLD;
     WorldInfo_Init(LWORLD);
+    int result = 0; 
+    if (NuIOS_IsLowEndDevice()) {
+        if (WORLD) {
+            char* inner_ptr = (char*)(WORLD->current_level);
+            if (inner_ptr) {
+                if (*(float*)(inner_ptr + 0x8C) < 20000.0f) {
+                    result = (*(float*)(inner_ptr + 0x90) < 20000.0f);
+                }
+            }
+        }
+    }
+    g_BackgroundUsedFogColour = result;
 }
 
 void WorldInfo_Init(WORLDINFO *info) {
