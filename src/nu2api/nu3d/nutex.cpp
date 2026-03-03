a37e599266fe642ba14b3de181681ddb998cff7d
pasdalover <me@pasdalover.ca>
2026-03-03 04:29:40 -0500

    NuDDSGetTextureDescription 70%

--- src/nu2api/nu3d/nutex.cpp
+++ src/nu2api/nu3d/nutex.cpp
@@ -1,11 +1,11 @@
 #include "nu2api/nu3d/nutex.h"
 
-#include <pthread.h>
-#include <string.h>
 #include "decomp.h"
 #include "nu2api/nu3d/android/nutex_android.h"
 #include "nu2api/nucore/common.h"
 #include "nu2api/nucore/nustring.h"
+#include <pthread.h>
+#include <string.h>
 
 void NuChecksumAsHex(u8 *checksum, char *out) {
     i32 i;
@@ -143,88 +143,156 @@ int NuTexHeight(int tex_id) {
     return texture_list[tex_id - 1]->height;
 }
 
-
-
 enum NUTEXFORMAT : unsigned int {
     NUTEX_DXT1 = 1,
-    NUTEX_DX1A = 2,       // DXT1 with Alpha (A1XD)
+    NUTEX_DX1A = 2, // DXT1 with Alpha (A1XD)
     NUTEX_DXT2 = 3,
     NUTEX_DXT3 = 4,
     NUTEX_DXT4 = 5,
     NUTEX_DXT5 = 6,
-    NUTEX_RGBA32 = 7,     // Calculated from (bVar3 * 8 + 7) for 32-bit
-    NUTEX_FLOAT16 = 8,    // 0x71 is D3DFMT_A16B16G16R16F
-    NUTEX_FLOAT32 = 9,    // 0x74 is D3DFMT_A32B32G32R32F
-    NUTEX_PAL8 = 12,      // 0xC  (PAL8)
-    NUTEX_PAL4 = 13,      // 0xD  (PAL4)
-    NUTEX_BANN = 14,      // 0xE  (BANN - likely a custom banner format)
-    NUTEX_RGB24 = 15,     // Calculated from (bVar3 * 8 + 7) for 24-bit
-    NUTEX_ETC1 = 17,      // 0x11 (ETC1)
-    NUTEX_ETCA = 18,      // 0x12 (ETC with Alpha)
-    NUTEX_PVRTC2 = 20,    // 0x14 (PT21)
-    NUTEX_PVRTC2A = 21,   // 0x15 (PT2A)
-    NUTEX_PVRTC4 = 22,    // 0x16 (PTC1)
-    NUTEX_PVRTC4A = 23,   // 0x17 (PTCA)
-    NUTEX_ATCA = 24,      // 0x18 (ACTA)
-    NUTEX_ATC = 25        // 0x19 (ATC)
+    NUTEX_RGBA32 = 7,   // Calculated from (bVar3 * 8 + 7) for 32-bit
+    NUTEX_FLOAT16 = 8,  // 0x71 is D3DFMT_A16B16G16R16F
+    NUTEX_FLOAT32 = 9,  // 0x74 is D3DFMT_A32B32G32R32F
+    NUTEX_PAL8 = 12,    // 0xC  (PAL8)
+    NUTEX_PAL4 = 13,    // 0xD  (PAL4)
+    NUTEX_BANN = 14,    // 0xE  (BANN - likely a custom banner format)
+    NUTEX_RGB24 = 15,   // Calculated from (bVar3 * 8 + 7) for 24-bit
+    NUTEX_ETC1 = 17,    // 0x11 (ETC1)
+    NUTEX_ETCA = 18,    // 0x12 (ETC with Alpha)
+    NUTEX_PVRTC2 = 20,  // 0x14 (PT21)
+    NUTEX_PVRTC2A = 21, // 0x15 (PT2A)
+    NUTEX_PVRTC4 = 22,  // 0x16 (PTC1)
+    NUTEX_PVRTC4A = 23, // 0x17 (PTCA)
+    NUTEX_ATCA = 24,    // 0x18 (ACTA)
+    NUTEX_ATC = 25      // 0x19 (ATC)
 };
 
-// _Z26NuDDSGetTextureDescriptionPKcR11NUTEXFORMATRiS3_S3_S3_RbPb
-/* NuDDSGetTextureDescription(char const*, NUTEXFORMAT&, int&, int&, int&, int&, bool&, bool*) */
 
-i32 NuDDSGetTextureDescription(const char *dds_buffer,NUTEXFORMAT &param_2,int &param_3,int &param_4,int &param_5,int &param_6,bool &param_7,bool *is_compressed)
+i32 NuDDSGetTextureDescription(const char *ddsData, NUTEXFORMAT &outFormat, int &outWidth, int &outHeight, int &outPitchOrLinearSize, int &outMipCount, bool &outIsCubeMap, bool *outHasFourCC)
 
 {
-  i32 bitfield;
-  i32 uVar2;
-  bool bVar3;
+    if (ddsData[0] != 'D')
+        return 0;
+    if (ddsData[1] != 'D')
+        return 0;
+    if (ddsData[2] != 'S')
+        return 0;
 
-  if (dds_buffer[0] != 'D' && dds_buffer[1] != 'D' && dds_buffer[2] != 'S') {
+    uint32_t fourCC = *(uint32_t *)(ddsData + 0x54);
+    *outHasFourCC = (fourCC != 0);
 
-    return 0;
-  }
-  uVar2 = dds_buffer[0x54];
-  *is_compressed = uVar2 != 0;
-  bitfield = dds_buffer[0x50];
-  if ((bitfield & 0x40) == 0) {
-    if ((bitfield & 0x20) != 0) {
-        param_2 = NUTEX_PAL8;
-    }
-    if ((bitfield & 8) != 0) {
-        param_2 = NUTEX_PAL4;
-    }
-    if (uVar2 == '3TXD') {
-        param_2 = NUTEX_DXT3;
-    }
-    if ('3TXD' < uVar2) {
-        if (uVar2 == 'A1XD') {
-            param_2 = NUTEX_DX1A;
-        } 
-        else if (uVar2 < 'A1XE') {
-            if (uVar2 == '4TXD') {
-            param_2 = NUTEX_DXT4;
-            } 
-            else if (uVar2 < '4TXE') {
-                if (uVar2 == '4LAP') {
-                    param_2 = NUTEX_PAL4;
-                }
-            }
-            else if (uVar2 == '5TXD') {
-                param_2 = NUTEX_DXT5;
-            }
-            else if (uVar2 == '8LAP') {
-                param_2 = NUTEX_PAL8;
+    if ((ddsData[0x50] & 0x40) == 0) {
+        if ((ddsData[0x50] & 0x20) != 0) {
+            outFormat = NUTEX_PAL8;
+        } else if ((ddsData[0x50] & 0x8) != 0) {
+            outFormat = NUTEX_PAL4;
+        } else {
+            switch (fourCC) {
+                case 0x31545844:
+                    outFormat = NUTEX_DXT1;
+                    break;
+                case 0x41315844:
+                    outFormat = NUTEX_DX1A;
+                    break;
+                case 0x32545844:
+                    outFormat = NUTEX_DXT2;
+                    break;
+                case 0x33545844:
+                    outFormat = NUTEX_DXT3;
+                    break;
+                case 0x34545844:
+                    outFormat = NUTEX_DXT4;
+                    break;
+                case 0x35545844:
+                    outFormat = NUTEX_DXT5;
+                    break;
+                case 0x00000071:
+                    outFormat = NUTEX_FLOAT16;
+                    break;
+                case 0x00000074:
+                    outFormat = NUTEX_FLOAT32;
+                    break;
+                case 0x344c4150:
+                    outFormat = NUTEX_PAL4;
+                    break;
+                case 0x384c4150:
+                    outFormat = NUTEX_PAL8;
+                    break;
+                case 0x4e4e4142:
+                    outFormat = NUTEX_BANN;
+                    break;
+                case 0x31435445:
+                    outFormat = NUTEX_ETC1;
+                    break;
+                case 0x41435445:
+                    outFormat = NUTEX_ETCA;
+                    break;
+                case 0x31325450:
+                    outFormat = NUTEX_PVRTC2;
+                    break;
+                case 0x41325450:
+                    outFormat = NUTEX_PVRTC2A;
+                    break;
+                case 0x31435450:
+                    outFormat = NUTEX_PVRTC4;
+                    break;
+                case 0x41435450:
+                    outFormat = NUTEX_PVRTC4A;
+                    break;
+                case 0x41435441:
+                    outFormat = NUTEX_ATCA;
+                    break;
+                case 0x20435441:
+                    outFormat = NUTEX_ATC;
+                    break;
+                case 0:
+                    outFormat = (NUTEXFORMAT)((*(uint32_t *)(ddsData + 0x58) != 0x20) * 8 + 7);
+                    break;
+                default:
+                    break;
             }
         }
-        else if (uVar2 == 'ACTE') {
-                param_2 = NUTEX_ETCA;
-            }
+    } else {
+        outFormat = (NUTEXFORMAT)((*(uint32_t *)(ddsData + 0x58) == 0x18) * 8 + 7);
     }
-    
 
+    outIsCubeMap = false;
+    outWidth = 0;
+    outHeight = 0;
+    outPitchOrLinearSize = 0;
 
+    outWidth = *(int *)(ddsData + 0x10);
+    outHeight = *(int *)(ddsData + 0x0C);
+
+    if ((ddsData[10] & 0x80) != 0) {
+        outPitchOrLinearSize = *(int *)(ddsData + 0x18);
+    }
+
+    uint32_t mipCountVal = *(uint32_t *)(ddsData + 0x1C);
+    uint32_t caps2 = *(uint32_t *)(ddsData + 0x70);
+
+    if (mipCountVal == 0) {
+        mipCountVal = (ddsData[10] & 2) == 0;
+    }
+    outMipCount = mipCountVal;
+
+    if ((ddsData[0x6C] & 8) == 0)
+        return 1;
+    if ((caps2 & 0x200) == 0)
+        return 1;
+    if ((caps2 & 0x1000) == 0)
+        return 1;
+    if ((caps2 & 0x400) == 0)
+        return 1;
+    if ((caps2 & 0x800) == 0)
+        return 1;
+    if ((caps2 & 0x4000) == 0)
+        return 1;
+    if ((caps2 & 0x8000) == 0)
+        return 1;
+    if ((caps2 & 0x2000) == 0)
+        return 1;
+
+    outIsCubeMap = true;
+    return 1;
 }
-
-  return 1;
-}
-
