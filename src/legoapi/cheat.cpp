da1ead89e0b4db63578920691642d243d88216ca
pasdalover <me@pasdalover.ca>
2026-03-11 03:29:31 -0400

    make cheats 99% accuracy

--- src/legoapi/cheat.cpp
+++ src/legoapi/cheat.cpp
@@ -2,6 +2,8 @@
 
 #include "nu2api/nucore/nustring.h"
 
+CHEATSYSTEM CheatSystem = {0};
+
 void Cheat_SetArea(i32 cheat, i32 areaId) {
     if (cheat >= 0 && cheat < CheatSystem.cheats_count && areaId >= 0 && areaId < AREACOUNT) {
         CheatSystem.cheats[cheat].area = areaId;
@@ -23,4 +25,16 @@ int Cheat_FindByName(char *name) {
         } while (i < CheatSystem.cheats_count);
     }
     return -1;
+}
+
+u32 Cheats_CheckFlags(u32 flag) {
+    return CheatSystem.flags & flag;
+}
+
+u32 Cheat_CheckFlags(int cheat_index, u32 flag_mask) {
+    if ((cheat_index > -1) && (cheat_index < CheatSystem.cheats_count)) {
+        CHEAT target_cheat = CheatSystem.cheats[cheat_index];
+        return flag_mask & target_cheat.flag;
+    }
+    return 0;
 }
--- src/legoapi/cheat.h
+++ src/legoapi/cheat.h
@@ -7,5 +7,18 @@
 void Cheat_SetArea(i32 cheat, i32 areaId);
 
 i32 Cheat_FindByName(char *name);
+u32 Cheats_CheckFlags(u32 flag);
+u32 Cheat_CheckFlags(int cheat_index, u32 flag_mask);
+struct cheatsystem_s {
+        CHEAT *cheats;
+        int cheats_count;
+        int flags;
+        undefined field3_0xc;
+        undefined field4_0xd;
+        undefined field5_0xe;
+        undefined field6_0xf;
+    };
+    typedef struct cheatsystem_s CHEATSYSTEM;
+
 
 #endif
