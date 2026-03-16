e49a45877dd7f6a6bdfe919d69a10f2716ed5024
pasdalover <amazingemail@email.email>
2026-03-16 07:46:10 -0400

    GetGLSLTypeInfo

--- src/nu2api/nu3d/nushader.cpp
+++ src/nu2api/nu3d/nushader.cpp
@@ -3,8 +3,22 @@
 #include "nu2api/nu3d/NuRenderDevice.h"
 #include "nu2api/nuandroid/ios_graphics.h"
 #include "nu2api/nucore/bgproc.h"
+#include "nu2api/nucore/nustring.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 static f32 water_theta_step = 0.26666668f;
+
+
+
+NUSHADERPROGRAM programPool[64];
+unsigned char g_shaderProgramBitmask[8];
+int g_shaderProgramCursor;
+NUUNIFORMRECORD g_uniformParameterRecordStorage[10];
+int g_uniformParameterRecordAllocator;
+GLuint g_boundShader;
+char BuildRegisterIndexToUniformLocationMapping_uniformName[256];
 
 void NuShaderObjectBaseCreate(NUSHADEROBJECTBASE *shader) {
     shader->field0 = -1;
@@ -145,4 +159,238 @@ int NuShaderObjectGenerateGLSLShader(GLuint *shader_dest, GLenum shader_type, co
     }
 
     return 1;
+}
+
+
+
+
+bool LinkShaderProgram(unsigned int a1)
+{
+  int v2;
+  glLinkProgram(a1);
+  glGetProgramiv(a1, 35714, &v2);
+  return v2 != 0;
+}
+
+
+
+GLSLTYPEINFO typeInfoTable[9]; 
+
+GLSLTYPEINFO* GetGLSLTypeInfo(GLenum glType) {
+    if (glType == GL_FLOAT)        return &typeInfoTable[0];
+    if (glType == GL_FLOAT_VEC2)   return &typeInfoTable[1];
+    if (glType == GL_FLOAT_VEC3)   return &typeInfoTable[2];
+    if (glType == GL_FLOAT_VEC4)   return &typeInfoTable[3];
+    if (glType == GL_FLOAT_MAT2)   return &typeInfoTable[4];
+    if (glType == GL_FLOAT_MAT3)   return &typeInfoTable[5];
+    if (glType == GL_FLOAT_MAT4)   return &typeInfoTable[6];
+    if (glType == GL_SAMPLER_2D)   return &typeInfoTable[7];
+    return (glType == GL_SAMPLER_CUBE) ? &typeInfoTable[8] : (GLSLTYPEINFO*)0;
+}
+
+
+
+static int GetHLSLRegisterIndex(const char *param_1, const char *param_2, bool param_3) {
+    char searchBuffer[264];
+    
+    char *globalUniformName = (char *)0x11ad9a1; 
+
+    sprintf(searchBuffer, "_%s ", globalUniformName);
+
+    char *constantsHeader = strstr((char*)param_1, "//NU2API CONSTANTS :");
+    char *uniformMatch = strstr((char*)param_1, searchBuffer);
+    char *attribsHeader = strstr((char*)param_1, "//NU2API ATTRIBS :");
+
+    if (constantsHeader != NULL && uniformMatch != NULL && 
+       (attribsHeader == NULL || uniformMatch < attribsHeader)) {
+        
+        int searchLen = NuStrLen(searchBuffer);
+        unsigned char *haystack = (unsigned char *)(uniformMatch + searchLen);
+
+        if (*param_2 == '\0') {
+            if (NuToUpper(*haystack) == 'C') {
+                haystack++;
+            }
+        } else {
+            int texLen = NuStrLen("TEXUNIT");
+            if (NuStrNICmp((char *)haystack, "TEXUNIT", texLen) == 0) {
+                haystack += texLen;
+            }
+        }
+
+        if (*haystack != 0xFF && isdigit(*haystack)){
+            int regIndex = NuAToI((char *)haystack);
+
+            unsigned char *nameMatch = (unsigned char *)strstr((char *)haystack, globalUniformName + 1);
+
+            if (nameMatch != NULL && haystack < nameMatch) {
+                unsigned char *cursor = nameMatch - 1;
+                
+
+                while (*cursor != '\n') { 
+                    int varLen = NuStrLen("//var ");
+                    
+                    if (NuStrNCmp((char *)cursor, "//var ", varLen) != 0) {
+                        return regIndex; 
+                    }
+                    
+                    if (cursor == haystack) {
+                        return -1;
+                    }
+                    
+                    cursor--;
+                }
+            }
+        }
+    }
+    
+    return -1; 
+}
+
+
+NUSHADERPROGRAM* NuShaderProgramCreateIOS(const GLchar *vtxSource, const GLchar *fragSource) {
+    GLuint vtxShader, fragShader, programId;
+    int compileStatus;
+    const char *precisionStr;
+    NUSHADERPROGRAM *prog;
+
+    vtxShader = glCreateShader(GL_VERTEX_SHADER);
+    precisionStr = strstr(vtxSource, "precision mediump float;");
+    if (precisionStr != NULL) {
+        memcpy((void*)precisionStr, "precision highp float;  ", NuStrLen("precision highp float;  "));
+    }
+    glShaderSource(vtxShader, 1, &vtxSource, NULL);
+    glCompileShader(vtxShader);
+    
+    compileStatus = 0;
+    glGetShaderiv(vtxShader, GL_COMPILE_STATUS, &compileStatus);
+    if (compileStatus == 0) glDeleteShader(vtxShader);
+
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    NuStrLen(fragSource);
+    glShaderSource(fragShader, 1, &fragSource, NULL);
+    glCompileShader(fragShader);
+    
+    compileStatus = 0;
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &compileStatus);
+    if (compileStatus == 0) {
+        glDeleteShader(fragShader);
+        return NULL;
+    }
+
+    programId = glCreateProgram();
+    glAttachShader(programId, vtxShader);
+    glAttachShader(programId, fragShader);
+    LinkShaderProgram(programId);
+    NuShaderObjectBindAttributeLocationsGLSL(programId);
+
+    int poolIndex = -1;
+    if (g_shaderProgramCursor < 64) {
+        int checkIdx = g_shaderProgramCursor;
+        int byteIdx = checkIdx >> 3;
+        int bitIdx = checkIdx & 7;
+        unsigned int bits = g_shaderProgramBitmask[byteIdx] >> bitIdx;
+        
+        while ((bits & 1) != 0) {
+            checkIdx++;
+            if (checkIdx == 64) {
+                if (g_shaderProgramCursor > 0) goto fallback_search;
+                goto alloc_fail;
+            }
+            byteIdx = checkIdx >> 3;
+            bitIdx = checkIdx & 7;
+            bits = g_shaderProgramBitmask[byteIdx] >> bitIdx;
+        }
+        g_shaderProgramBitmask[byteIdx] |= (1 << bitIdx);
+        g_shaderProgramCursor = (checkIdx + 1) % 64;
+        poolIndex = checkIdx;
+    } else {
+    fallback_search:
+        if ((g_shaderProgramBitmask[0] & 1) == 0) {
+            g_shaderProgramBitmask[0] |= 1;
+            g_shaderProgramCursor = 1 % 64;
+            poolIndex = 0;
+        } else {
+            int checkIdx = 0;
+            unsigned int bits;
+            do {
+                checkIdx++;
+                if (checkIdx == g_shaderProgramCursor) goto alloc_fail;
+                bits = g_shaderProgramBitmask[checkIdx >> 3] >> (checkIdx & 7);
+            } while ((bits & 1) != 0);
+            
+            g_shaderProgramBitmask[checkIdx >> 3] |= (1 << (checkIdx & 7));
+            g_shaderProgramCursor = (checkIdx + 1) % 64;
+            poolIndex = checkIdx;
+        }
+    }
+
+    if (poolIndex == -1) {
+    alloc_fail:
+        return NULL; 
+    }
+
+    prog = &programPool[poolIndex];
+    prog->vertexShader = vtxShader;
+    prog->fragmentShader = fragShader;
+    prog->program = programId;
+
+    glGetProgramiv(programId, GL_ACTIVE_UNIFORMS, &prog->numUniforms);
+    
+    if (prog->numUniforms == 0) {
+        prog->uniforms = NULL;
+    } else {
+        int allocStart = g_uniformParameterRecordAllocator;
+        g_uniformParameterRecordAllocator += prog->numUniforms;
+        prog->uniforms = &g_uniformParameterRecordStorage[allocStart];
+        
+        int skippedUniforms = 0;
+        
+        for (int i = 0; i < prog->numUniforms; i++) {
+            GLsizei length; GLint size; GLenum type;
+            char *uniformName = BuildRegisterIndexToUniformLocationMapping_uniformName;
+            
+            glGetActiveUniform(prog->program, i, 0x100, &length, &size, &type, uniformName);
+            
+            char *bracket = strchr(uniformName, '[');
+            if (bracket != NULL) *bracket = '\0';
+
+            NUUNIFORMRECORD *record = &prog->uniforms[i - skippedUniforms];
+            unsigned short shaderFlag = 0;
+            
+            int hlslReg = GetHLSLRegisterIndex((const char*)vtxSource, (const char*)0, false);
+            if (hlslReg == -1) {
+                hlslReg = GetHLSLRegisterIndex((const char*)fragSource, (const char*)0, true);
+                shaderFlag = 0x8000;
+            }
+            
+            record->registerInfo = shaderFlag | (unsigned short)hlslReg;
+            
+            int glLoc = glGetUniformLocation(prog->program, uniformName);
+            record->locationInfo = (record->locationInfo & 0xF000) | (glLoc & 0x0FFF);
+            
+            GLSLTYPEINFO *typeInfo = GetGLSLTypeInfo(type);
+            
+            if (typeInfo != NULL) {
+                if (typeInfo->typeClass == 4) { 
+                    int texReg = GetHLSLRegisterIndex((const char*)fragSource, (const char*)1, true);
+                    if (texReg != -1) {
+                        glUseProgram(prog->program);
+                        glUniform1i(glLoc, texReg);
+                        glUseProgram(0);
+                        g_boundShader = 0;
+                    }
+                    skippedUniforms++;
+                    continue;
+                }
+                
+
+                ((unsigned char*)record)[3] = (((unsigned char*)record)[3] & 0x0F) | (typeInfo->typeEnum << 4);
+            }
+        }
+        prog->numUniforms -= skippedUniforms;
+        g_uniformParameterRecordAllocator -= skippedUniforms;
+    }
+
+    return prog;
 }
