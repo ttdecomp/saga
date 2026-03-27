069ba7ea4293d9ec3d59421b7fcf803361ef5d4b
Fabian Lippold <fabianlippold1184@gmail.com>
2026-03-27 05:18:03 +0100

    refactor build

---
+++ host-tests/nuios/nutex_host.cpp
@@ -0,0 +1,13 @@
+#include "nu2api/nu3d/android/nutex_android.h"
+
+#include "nu2api/nu3d/android/nutex_ios_ex.h"
+
+#include "decomp.h"
+
+int NuTexRead(char *name, VARIPTR *buf, VARIPTR *buf_end) {
+    UNIMPLEMENTED();
+}
+
+void NuTexCreatePS(NUNATIVETEX *tex, bool is_pvrtc) {
+    UNIMPLEMENTED();
+}
